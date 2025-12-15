13eb2230c1e63d8add569c3654be144b40d02700
Глеб <hwrfp8285q@privaterelay.appleid.com>
2025-12-15 14:47:18 +0300

    lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -7,45 +7,58 @@ protected:
     string brand;
     int cylinders;
     double power;
+
 public:
     Car() : brand("Unknown"), cylinders(4), power(100) {}
-    Car(string b, int c, double p) : brand(b), cylinders(c), power(p) {
-        if (c <= 0 || p <= 0) { cout << "Ошибка в машине!\n"; exit(1); }
+
+    friend istream& operator>>(istream& in, Car& c) {
+        cout << "Марка машины: ";
+        in >> c.brand;
+        cout << "Кол-во цилиндров: ";
+        in >> c.cylinders;
+        cout << "Мощность (л.с.): ";
+        in >> c.power;
+
+        if (c.cylinders <= 0 || c.power <= 0) {
+            cout << "Ошибка!\n";
+            exit(1);
+        }
+        return in;
     }
 
-    void input() {
-        cout << "Марка машины: "; cin >> brand;
-        cout << "Кол-во цилиндров: "; cin >> cylinders;
-        cout << "Мощность (л.с.): "; cin >> power;
-        if (cylinders <= 0 || power <= 0) { cout << "Ошибка!\n"; exit(1); }
+    friend ostream& operator<<(ostream& out, const Car& c) {
+        out << "Машина: " << c.brand
+            << ", цилиндры: " << c.cylinders
+            << ", мощность: " << c.power << " л.с.\n";
+        return out;
     }
 
-    void setPower(double p) { if (p > 0) power = p; else exit(1); }
-
-    void show() const {
-        cout << "Машина: " << brand << ", цилиндры: " << cylinders << ", мощность: " << power << " л.с.\n";
-    }
-
-    string getBrand() const { return brand; }
+    virtual ~Car() {}
 };
 
 class Lorry : public Car {
     double loadCapacity;
+
 public:
     Lorry() : Car(), loadCapacity(0) {}
-    Lorry(string b, int c, double p, double l) : Car(b, c, p), loadCapacity(l) {
-        if (l < 0) { cout << "Ошибка грузоподъёмности!\n"; exit(1); }
+
+    // ОПЕРАЦИЯ ВВОДА
+    friend istream& operator>>(istream& in, Lorry& l) {
+        in >> static_cast<Car&>(l);
+        cout << "Грузоподъёмность (тонн): ";
+        in >> l.loadCapacity;
+
+        if (l.loadCapacity < 0) {
+            cout << "Ошибка!\n";
+            exit(1);
+        }
+        return in;
     }
 
-    void input() {
-        Car::input();
-        cout << "Грузоподъёмность (тонн): "; cin >> loadCapacity;
-        if (loadCapacity < 0) { cout << "Ошибка!\n"; exit(1); }
-    }
-
-    void show() const {
-        Car::show();
-        cout << "Грузоподъёмность: " << loadCapacity << " тонн\n\n";
+    friend ostream& operator<<(ostream& out, const Lorry& l) {
+        out << static_cast<const Car&>(l);
+        out << "Грузоподъёмность: " << l.loadCapacity << " тонн\n";
+        return out;
     }
 };
 
@@ -54,19 +67,21 @@ int main() {
     Lorry truck;
 
     cout << "Ввод обычной машины:\n";
-    car.input();
+    cin >> car;
 
-    cout << "Ввод грузовика:\n";
-    truck.input();
+    cout << "\nВвод грузовика:\n";
+    cin >> truck;
 
-    cout << "Результат\n";
-    car.show();
-    truck.show();
+    cout << "\nОбычная машина:\n";
+    cout << car;
+
+    cout << "\nГрузовик:\n";
+    cout << truck;
 
     Car* ptr = &truck;
-    cout << "Через указатель на Car:\n";
-    ptr->show();
+    cout << "\nЧерез указатель на Car:\n";
+    cout << *ptr;
 
-    system("pause");
     return 0;
 }
+
